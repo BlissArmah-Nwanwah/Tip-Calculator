1ecc4ba9681dae92d97fa489c6079508e395fc1e
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-01-16 08:29:27 +0000

    functionalities started

--- src/page.jsx
+++ src/page.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import image from "./assets/logo.svg";
 import dollar from "./assets/icon-dollar.svg";
+import { useState } from "react";
 
 const page = () => {
+  const [bill,setBill] = useState(20)
+  const [button,setButton] =useState(0)
+  const [tip,setTip] =useState(0)
+  const handleButton =(value)=>{
+    setButton(value / 100)
+    setTip(bill*button)
+  }
+  
+  console.log(tip);
+ 
   return (
     <main>
       <img src={image} alt="" className="logo" />
@@ -11,17 +22,17 @@ const page = () => {
           <div className="bill">
             <span>BILL</span>
             <div>
-              <input type="text" className="bill-icon" />
+              <input type="text" className="bill-icon" value={bill} onChange={(e)=>setBill(e.target.value)} />
             </div>
           </div>
           <div className="select-tip">
             <span>Select Tip %</span>
             <div className="tip-button">
-              <button>5%</button>
-              <button>10%</button>
-              <button className="default-btn">15%</button>
-              <button>25%</button>
-              <button>50%</button>
+              <button onClick={()=>handleButton('5')}>5%</button>
+              <button onClick={()=>handleButton('10')}>10%</button>
+              <button className="default-btn" onClick={()=>handleButton('15')}>15%</button>
+              <button onClick={()=>handleButton('25')}>25%</button>
+              <button onClick={()=>handleButton('50')}>50%</button>
               <input type="text" placeholder="Custom" className="select-input"/>
             </div>
           </div>
@@ -38,7 +49,7 @@ const page = () => {
               <span>/ person</span>
             </div>
             
-            <div className="figure">$4.27</div>
+            <div className="figure">${tip}</div>
           </div>
           <div className="total-amount">
             <div className="text">
